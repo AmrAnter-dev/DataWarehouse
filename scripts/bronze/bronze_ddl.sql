a4ca7417678421020566ec8e9b3854b9a3e6b6d5
AmrAnter-dev <mavireck1983@gmail.com>
2026-04-25 19:20:25 +0300

    Add files via upload

---
+++ scripts/bronze/bronze_ddl.sql
@@ -0,0 +1,119 @@
+USE DataWarehouse;
+GO
+
+IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+GO
+CREATE TABLE bronze.crm_cust_info
+        (
+                cst_id             int          ,
+                cst_key            nvarchar(50)  ,
+                cst_firstname      nvarchar(50)  ,
+                cst_lastname       nvarchar(50)  ,
+                cst_marital_status nvarchar(10)  ,
+                cst_gndr           nvarchar(10)  ,
+                cst_create_date    nvarchar(50),
+                -- Lineage / Audit columns
+                dwh_batch_id       bigint,
+                dwh_record_source  nvarchar(50)  default 'CRM',
+                dwh_load_date      date default cast(GETDATE() as date),
+                dwh_ingestion_ts   datetime2 default SYSDATETIME()
+               
+        )
+;
+GO
+IF OBJECT_ID('bronze.crm_prd_info','U') IS NOT NULL
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+GO
+CREATE TABLE bronze.crm_prd_info
+        (
+                prd_id           int          ,
+                prd_key          nvarchar(50)  ,
+                prd_nm           nvarchar(50)  ,
+                prd_cost         decimal(10,2),
+                prd_line         nvarchar(20) ,
+                prd_start_dt     nvarchar(20)         ,
+                prd_end_dt       nvarchar(20)         ,
+                -- Lineage / Audit columns
+                dwh_batch_id       bigint,
+                dwh_record_source  nvarchar(50)  default 'CRM',
+                dwh_load_date      date default cast(GETDATE() as date),
+                dwh_ingestion_ts   datetime2 default SYSDATETIME()
+             
+        )
+;
+GO
+IF OBJECT_ID('bronze.crm_sales_details','U') IS NOT NULL
+DROP TABLE IF EXISTS bronze.crm_sales_details;
+GO
+CREATE TABLE bronze.crm_sales_details
+        (
+                sls_ord_num      nvarchar(50) ,
+                sls_prd_key      nvarchar(50) ,
+                sls_cust_id      INT          ,
+                sls_order_dt     nvarchar(20)         ,
+                sls_ship_dt      nvarchar(20)         ,
+                sls_due_dt       nvarchar(20)         ,
+                sls_sales        decimal(10,2),
+                sls_quantity     INT          ,
+                sls_price        INT,
+               -- Lineage / Audit columns
+                dwh_batch_id       bigint,
+                dwh_record_source  nvarchar(50) default 'CRM',
+                dwh_load_date      date default cast(GETDATE() as date),
+                dwh_ingestion_ts   datetime2 default SYSDATETIME()
+            
+        )
+;
+GO
+IF OBJECT_ID('bronze.erp_cust_aZ12','U') IS NOT NULL
+DROP TABLE IF EXISTS bronze.erp_cust_aZ12;
+GO
+CREATE TABLE bronze.erp_cust_aZ12
+        (
+                cid              nvarchar(50) ,
+                bdate            nvarchar(20) ,
+                gen              nvarchar(50) ,
+                -- Lineage / Audit columns
+                dwh_batch_id       bigint,
+                dwh_record_source  nvarchar(50) default 'ERP' ,
+                dwh_load_date      date default cast(GETDATE() as date),
+                dwh_ingestion_ts   datetime2 default SYSDATETIME()
+               
+        )
+;
+GO
+IF OBJECT_ID('bronze.erp_loc_a101','U') IS NOT NULL
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+GO
+CREATE TABLE bronze.erp_loc_a101
+        (
+                cid              NVARCHAR(50) ,
+                cntry            NVARCHAR(25) ,
+                -- Lineage / Audit columns
+                dwh_batch_id       bigint,
+                dwh_record_source  nvarchar(50) default 'ERP' ,
+                dwh_load_date      date default cast(GETDATE() as date),
+                dwh_ingestion_ts   datetime2 default SYSDATETIME()
+        )
+;
+GO
+IF OBJECT_ID('bronze.erp_px_cat_g1v2','U') IS NOT NULL
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+GO
+CREATE TABLE bronze.erp_px_cat_g1v2
+        (
+                id               NVARCHAR(50) ,
+                cat              NVARCHAR(50) ,
+                subcat           NVARCHAR(50) ,
+                maintenance      NVARCHAR(25) ,
+                -- Lineage / Audit columns
+                dwh_batch_id       bigint,
+                dwh_record_source  nvarchar(50)  default 'ERP',
+                dwh_load_date      date default cast(GETDATE() as date),
+                dwh_ingestion_ts   datetime2 default SYSDATETIME()
+                
+        )
+;
+GO
+
